3f0ce00fa718dcf1935129e72eeb90209b609457
Bvishwa23 <167072428+Bvishwa23@users.noreply.github.com>
2026-09-10 10:10:56 +0530

    Modify date selection in Day3_Task_2
    Updated date selection logic to select October 6th, 2026.

--- Day3/Day3_Task_2.java
+++ Day3/Day3_Task_2.java
@@ -9,21 +9,21 @@ import java.time.Duration;
 
 public class Day3_Task_2 {
     public static void main(String[] args) {
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://demoapps.qspiders.com/ui/datePick?sublist=0");
-        driver.findElement(By.cssSelector("[type='text']")).click();
-        driver.findElement(By.cssSelector("[type='button']")).click();
-        driver.findElement(By.cssSelector("[role='option']")).click();
-        WebElement result=  driver.findElement(By.cssSelector("[type='text']"));
-        result.getDomAttribute("value");
-        if(result.getDomAttribute("value").contains("26/07/2026")) {
-            System.out.println("Date is displayed Correctly");
-
-        }else {
-            System.out.println("Date is not displayed Correctly");
-        }
+       WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demoapps.qspiders.com/ui/datePick?sublist=0");
+		driver.findElement(By.cssSelector("[type='text']")).click(); //click on calender
+		driver.findElement(By.cssSelector("[aria-label='Next Month']")).click(); //For selecting next month
+		driver.findElement(By.cssSelector("[aria-label='Choose Tuesday, October 6th, 2026']")).click(); //For selecting date
+		WebElement result=  driver.findElement(By.cssSelector("[type='text']"));  
+		result.getDomAttribute("value");
+		if(result.getDomAttribute("value").contains("06/10/2026")) {
+			System.out.println("Date is displayed Correctly");
+			
+		}else {
+			System.out.println("Date is not displayed Correctly");
+		}
 
 
     }
